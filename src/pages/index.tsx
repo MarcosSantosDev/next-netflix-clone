55d8e7da3848a857c3e1c4e1772a429abf0d4f6a
MarcosSantosDev <marcosadriano034@gmail.com>
2023-11-19 12:50:49 -0300

    feat: start project

---
+++ src/pages/index.tsx
@@ -0,0 +1,12 @@
+import Head from 'next/head';
+
+export default function Home() {
+  return (
+    <div>
+      <Head>
+        <title>Netflix clone</title>
+      </Head>
+      <h1 className="text-2xl text-white">Home - Nextflix</h1>
+    </div>
+  );
+}
