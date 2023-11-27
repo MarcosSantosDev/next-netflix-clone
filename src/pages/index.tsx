2ebb1abf6c496ff185159303e93c96d529cda66d
MarcosSantosDev <marcosadriano034@gmail.com>
2023-11-26 23:04:31 -0300

    feat: create billboard component

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,7 +2,7 @@ import Head from 'next/head';
 import { getSession } from 'next-auth/react';
 import { GetServerSideProps } from 'next';
 
-import { Navbar } from '@/components/context';
+import { Navbar, Billboard } from '@/components/context';
 
 export const getServerSideProps: GetServerSideProps = async context => {
   const session = await getSession(context);
@@ -28,31 +28,7 @@ export default function Home() {
         <title>Netflix clone</title>
       </Head>
       <Navbar />
-      <div className="bg-gray-500">
-        <div className="h-96"></div>
-        <div className="h-96"></div>
-        <div className="h-96"></div>
-        <div className="h-96"></div>
-        <div className="h-96"></div>
-        <div className="h-96"></div>
-        <div className="h-96"></div>
-        <div className="h-96"></div>
-        <div className="h-96"></div>
-        <div className="h-96"></div>
-        <div className="h-96"></div>
-        <div className="h-96"></div>
-        <div className="h-96"></div>
-        <div className="h-96"></div>
-        <div className="h-96"></div>
-        <div className="h-96"></div>
-        <div className="h-96"></div>
-        <div className="h-96"></div>
-        <div className="h-96"></div>
-        <div className="h-96"></div>
-        <div className="h-96"></div>
-        <div className="h-96"></div>
-        <div className="h-96"></div>
-      </div>
+      <Billboard />
     </>
   );
 }
